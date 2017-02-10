76094df7ed826e3955ce0f1784754428da9182e4
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-10 15:09:13 +0530

    Done with about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there")
-end
+	return HttpResponse("Rango says hey there index")
 
-
+def about(request):
+	return HttpResponse("Rango says hey there about")
