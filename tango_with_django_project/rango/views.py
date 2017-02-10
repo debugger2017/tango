c2a9f1d9a326923f42ee526753118dfb5b643c86
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-10 16:29:19 +0530

    Added html for index

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there index")
-
+	context_dict = {'boldmessage' : "I am bold message"}
+	return render(request,'rango/index.html',context_dict)
 def about(request):
 	return HttpResponse("Rango says hey there about")
